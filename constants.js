4b3511f748910a00b22235a0d8e69271e6ebb27c
Pablo CANEPA <pprofpc@gmail.com>
2022-11-01 18:32:52 -0300

    Conexión Mongo Atlas

---
+++ constants.js
@@ -0,0 +1,16 @@
+const DB_USER = "admin";
+const DB_PASSWORD = "admin123456";
+const DB_HOST = "web-personal.duntxzd.mongodb.net";
+
+const API_VERSION = "V1";
+const IP_SERVER = "localhost";
+
+module.exports = {
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    API_VERSION,
+    IP_SERVER,
+}
+
+//mongodb+srv://admin:<password>@web-personal.duntxzd.mongodb.net/?retryWrites=true&w=majority
--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const mongoose = require("mongoose");
-const app = require('./app');
-const PORT_SERVER = process.env.PORT || 3977;
-const { API_VERSION, IP_SERVER, PORT_DB } = require('./config');
+const { 
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    IP_SERVER,
+    API_VERSION,  } = require("./constants");
 
-//mongoose.set("useFindAndModify", false);
+    mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`,
+(err) => {
+    if (err) throw err;
 
-mongoose.connect(`mongodb://${IP_SERVER}:${PORT_DB}/webpersonal`,
-{useNewUrlParser: true},
-(err, res) => {
-    if(err){
-        throw err;
-    } else {
-        console.log("DB connected :-)");
-
-        app.listen(PORT_SERVER, () =>{
-            console.log("###############");
-            console.log("## API REST ###");
-            console.log("###############");
-            console.log(`http://${IP_SERVER}:${PORT_SERVER}/api/${API_VERSION}/`);
-        });
-    }
-});
+    console.log("La conexión con la BD ha sido exitosa");
+}
+    );
