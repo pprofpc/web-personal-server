3c1088a1682d9b7342dc4904a238a80b39933230
Pablo CANEPA <pprofpc@gmail.com>
2023-05-26 16:34:10 -0300

    Login

--- constants.js
+++ constants.js
@@ -5,12 +5,15 @@ const DB_HOST = "web-personal.duntxzd.mongodb.net";
 const API_VERSION = "v1";
 const IP_SERVER = "localhost";
 
+const JWT_SECRET_KEY = "cualquier123string4567sECREt";
+
 module.exports = {
     DB_USER,
     DB_PASSWORD,
     DB_HOST,
     API_VERSION,
     IP_SERVER,
+    JWT_SECRET_KEY,
 }
 
 //mongodb+srv://admin:<password>@web-personal.duntxzd.mongodb.net/?retryWrites=true&w=majority
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,24 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+const jwt = require('../utils/jwt');
 
-function register(req, res){
-    const{firstname, lastname, email, password} = req.body;
+function register(req, res) {
+    const { firstname, lastname, email, password } = req.body;
     //console.log("Se ha ejecutado el registro");
     //console.log(req.body);
 
-    if(!email || !password){
+    if (!email || !password) {
         res.status(404).send({ message: "Los datos son obligatorios." });
     }
 
     const user = new User({
-        firstname, 
-        lastname, 
+        firstname,
+        lastname,
         email: email.toLowerCase(),
         role: "user",
         active: false,
-        password});
+        password
+    });
 
     const salt = bcrypt.genSaltSync(10);
     const hashPassword = bcrypt.hashSync(password, salt);
@@ -24,13 +26,13 @@ function register(req, res){
     user.password = hashPassword;
 
     user.save((err, userStored) => {
-        if(err){
-            res.status(500).send({message: "Error, el usuario ya existe: ",err});
+        if (err) {
+            res.status(500).send({ message: "Error, el usuario ya existe: ", err });
         } else {
-            if(!userStored){
-                res.status(404).send({message: "Error al crear el usuario"});
+            if (!userStored) {
+                res.status(404).send({ message: "Error al crear el usuario" });
             } else {
-                res.status(200).send({message: "TODO OK", user: userStored});
+                res.status(200).send({ message: "TODO OK", user: userStored });
             }
         }
     })
@@ -40,6 +42,43 @@ function register(req, res){
     //res.status(200).send({ msg: "TODO OK" });
 }
 
+function login(req, res) {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).send({ message: "Email and password are required." });
+      return;
+    }
+  
+    User.findOne({ email: email.toLowerCase() }, (err, user) => {
+      if (err) {
+        res.status(500).send({ message: "Internal server error." });
+        return;
+      }
+  
+      if (!user) {
+        res.status(404).send({ message: "User not found." });
+        return;
+      }
+  
+      bcrypt.compare(password, user.password, (bcryptErr, matched) => {
+        if (bcryptErr || !matched) {
+          res.status(401).send({ message: "Invalid email or password." });
+          return;
+        }
+  
+        if (!user.active) {
+          res.status(403).send({ message: "User is not active." });
+          return;
+        }
+  
+        // const token = jwt.createAccessToken(user);
+        res.status(200).send({ message: "Authentication successful." });
+      });
+    });
+  }
+  
+
 module.exports = {
     register,
+    login
 };
